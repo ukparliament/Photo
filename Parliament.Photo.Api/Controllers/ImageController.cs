9bee63cc4f151c0243187c6062bf05f84143762b
Samu Lang <langsamu@hotmail.com>
2017-07-12 21:45:07 +0100

    Added telemetry.

--- Parliament.Photo.Api/Controllers/ImageController.cs
+++ Parliament.Photo.Api/Controllers/ImageController.cs
@@ -1,8 +1,10 @@
 ﻿namespace Parliament.Photo.Api.Controllers
 {
     using ImageMagick;
+    using Microsoft.ApplicationInsights;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Blob;
+    using System.Collections.Generic;
     using System.Configuration;
     using System.Linq;
     using System.Net;
@@ -16,8 +18,10 @@
     [ImageControllerConfiguration]
     public class ImageController : ApiController
     {
+        private TelemetryClient telemetry = new TelemetryClient();
+
         /// <param name="quality"><seealso cref="https://www.imagemagick.org/script/command-line-options.php#quality"/></param>
-        public async Task<HttpResponseMessage> Get(string id, int? width = null, int? height = null, string crop = null, bool? download = null, int? quality = 100)
+        public async Task<HttpResponseMessage> Get(string id, int? width = null, int? height = null, string crop = null, bool? download = null, int? quality = null)
         {
             var image = await GetImage(id);
 
@@ -25,14 +29,20 @@
             Resize(image, width, height);
             AddMetadata(id, image);
 
-            image.Quality = quality.GetValueOrDefault(100);
+            if (quality != null)
+            {
+                image.Quality = quality.Value;
+
+                telemetry.TrackEvent(
+                    "Resized",
+                        metrics: new Dictionary<string, double> {
+                        { "quality", (long)quality }
+                    });
+            }
 
             var response = Request.CreateResponse(image);
 
-            if (download.GetValueOrDefault())
-            {
-                Download(id, response);
-            }
+            Download(id, download, response);
 
             return response;
         }
@@ -50,8 +60,13 @@
             }
         }
 
-        private static void Resize(MagickImage magick, int? width, int? height)
+        private void Resize(MagickImage magick, int? width, int? height)
         {
+            if (width == null && height == null)
+            {
+                return;
+            }
+
             var size = new MagickGeometry(
                 width.GetValueOrDefault(magick.Width),
                 height.GetValueOrDefault(magick.Height));
@@ -60,9 +75,16 @@
             magick.Scale(size);
             magick.RePage();
             magick.ColorSpace = ColorSpace.sRGB;
+
+            telemetry.TrackEvent(
+                "Resized",
+                metrics: new Dictionary<string, double> {
+                    { "width", size.Width },
+                    { "height", size.Height},
+                });
         }
 
-        private static void Crop(MagickImage magick, int x, int y, string crop)
+        private void Crop(MagickImage magick, int x, int y, string crop)
         {
             switch (crop)
             {
@@ -92,9 +114,19 @@
                             Content = new StringContent("Allowed crop values:\n\tMCU_3:4\n\tMCU_3:2\n\tCU_1:1\n\tCU_5:2")
                         });
             }
+
+            telemetry.TrackEvent(
+                "Cropped",
+                new Dictionary<string, string> {
+                    { "crop", crop }
+                },
+                new Dictionary<string, double> {
+                    { "x", x },
+                    { "y", y},
+                });
         }
 
-        private static void AddMetadata(string id, MagickImage magick)
+        private void AddMetadata(string id, MagickImage magick)
         {
             using (var metadataController = new MetadataController())
             {
@@ -104,19 +136,26 @@
 
                 magick.AddProfile(xmpProfile);
             }
+
+            telemetry.TrackEvent("Added metadata");
         }
 
-        private static void Download(string id, HttpResponseMessage response)
+        private void Download(string id, bool? download, HttpResponseMessage response)
         {
-            var content = response.Content as ObjectContent;
-            var formatterMapping = content.Formatter.MediaTypeMappings.First();
-            var mediaType = formatterMapping.MediaType.MediaType;
-            var mapping = Global.mappingData.Single(row => row.MediaType == mediaType);
-            var extension = mapping.Extensions.First();
-            var fileName = string.Format("{0}.{1}", id, extension);
-            var disposition = new ContentDispositionHeaderValue("attachment") { FileName = fileName };
+            if (download.GetValueOrDefault())
+            {
+                var content = response.Content as ObjectContent;
+                var formatterMapping = content.Formatter.MediaTypeMappings.First();
+                var mediaType = formatterMapping.MediaType.MediaType;
+                var mapping = Global.mappingData.Single(row => row.MediaType == mediaType);
+                var extension = mapping.Extensions.First();
+                var fileName = string.Format("{0}.{1}", id, extension);
+                var disposition = new ContentDispositionHeaderValue("attachment") { FileName = fileName };
 
-            content.Headers.ContentDisposition = disposition;
+                content.Headers.ContentDisposition = disposition;
+
+                telemetry.TrackEvent("Download");
+            }
         }
 
         private async static Task<CloudBlob> GetRawSource(string id)
