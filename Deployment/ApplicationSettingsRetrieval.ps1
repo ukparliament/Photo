4a6511ffc0d878fc896b2797e6171a9c634901e0
hellikopter <hellikopter@users.noreply.github.com>
2017-09-18 10:10:52 +0100

    update deployment

--- Deployment/ApplicationSettingsRetrieval.ps1
+++ Deployment/ApplicationSettingsRetrieval.ps1
@@ -3,7 +3,7 @@
 Get settings for API app.
 
 .DESCRIPTION
-Retrieves value of Subscription Key and IP of API Management and sets task variables.
+Retrieves value of API Management and sets task variable.
 
 .PARAMETER APIResourceGroupName
 Name of the Resource Group where the API Management is.
@@ -13,7 +13,8 @@ This script is for use as a part of deployment in VSTS only.
 #>
 
 Param(
-	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName
+	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
+	[Parameter(Mandatory=$true)] [string] $APIManagementName
 )
 $ErrorActionPreference = "Stop"
 
@@ -22,9 +23,9 @@ function Log([Parameter(Mandatory=$true)][string]$LogText){
 }
 
 Log "Get API Management"
-$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $APIResourceGroupName
+$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $APIResourceGroupName -Name $APIManagementName
 
 Log "Setting variables to use during deployment"
 Write-Host "##vso[task.setvariable variable=APIManagementIP]$($apiManagement.StaticIPs[0])"
 
-Log "Job wel done!"
+Log "Job well done!"
