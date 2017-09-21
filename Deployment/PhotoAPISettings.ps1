47cd1619797d6c4f681fe2a0805c1c91162ea80a
hellikopter <hellikopter@users.noreply.github.com>
2017-09-21 14:57:38 +0100

    deployment scripts split to infrastructure and code

---
+++ Deployment/PhotoAPISettings.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+Sets settings for API app.
+
+.DESCRIPTION
+Sets values of application settings.
+
+.PARAMETER APIResourceGroupName
+Name of the Resource Group where the API Management is.
+
+.NOTES
+This script is for use as a part of deployment in VSTS only.
+#>
+
+Param(
+	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
+	[Parameter(Mandatory=$true)] [string] $APIManagementName,
+	[Parameter(Mandatory=$true)] [string] $PhotoAPIName,
+	[Parameter(Mandatory=$true)] [string] $APIPrefix
+)
+$ErrorActionPreference = "Stop"
+
+function Log([Parameter(Mandatory=$true)][string]$LogText){
+    Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
+}
+
+Log "Get API Management context"
+$management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $APIManagementName
+Log "Retrives subscription"
+$apiProduct=Get-AzureRmApiManagementProduct -Context $management -Title "$APIPrefix - Parliament [Photo]"
+$subscription=Get-AzureRmApiManagementSubscription -Context $management -ProductId $apiProduct.ProductId
+
+Log "Gets current settings"
+$webApp = Get-AzureRmwebApp -ResourceGroupName $APIResourceGroupName -Name $FixedQueryName
+$webAppSettings = $webApp.SiteConfig.AppSettings
+$settings=@{}
+foreach($set in $webAppSettings){ 
+    $settings[$set.Name]=$set.Value
+}
+
+"https://$APIManagementName.azure-api.net/$APIPrefix/sparql-endpoint/master?subscription-key=$subscription.PrimaryKey"
+
+Log "Job well done!"
